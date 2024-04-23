371af3fc9080dd63acc9fd7b7abcb3d8808d4f1a
AdrianLiraMcfly <jorge.liralopez11@gmail.com>
2024-04-23 13:24:43 -0600

    Web

--- resources/js/Pages/Forum/canales.vue
+++ resources/js/Pages/Forum/canales.vue
@@ -1,11 +1,59 @@
 <template>
     <AuthenticatedLayout>
         <div class="container mx-auto mt-5">
-            <div class="flex justify-between items-center">
-                <h1 class="text-2xl font-bold">Canales</h1>
-                
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">Canales</div>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Nombre</th>
+                                        <th>Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="canal in canales" :key="canal.id">
+                                        <td>{{ canal.nombre }}</td>
+                                        <td>
+                                            <button-modal id_modal="modalEditarCanal" class="btn btn-warning">Editar</button-modal>
+                                            <button-modal id_modal="modalEliminarCanal" class="btn btn-danger">Eliminar</button-modal>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+        <Modal id="modalCrearCanal" titulo="Crear Canal">
+    <form @submit.prevent="crearCanal">
+        <div class="form-group">
+            <label for="nombre">Nombre</label>
+            <input type="text" class="form-control" id="nombre" v-model="form.nombre">
+        </div>
+        <button type="submit" class="btn btn-primary">Crear Canal</button>
+    </form>
+</Modal>
+
+<Modal id="modalEditarCanal" titulo="Editar Canal">
+    <form @submit.prevent="editarCanal">
+        <div class="form-group">
+            <label for="nombre">Nombre</label>
+            <input type="text" class="form-control" id="nombre" v-model="editform.nombre">
+        </div>
+        <button type="submit" class="btn btn-warning">Editar Canal</button>
+    </form>
+</Modal>
+
+<Modal id="modalEliminarCanal" titulo="Eliminar Canal">
+    <form @submit.prevent="eliminarCanal">
+        <p>¿Estás seguro de que deseas eliminar este canal?</p>
+        <button type="submit" class="btn btn-danger">Eliminar Canal</button>
+    </form>
+</Modal>
     </AuthenticatedLayout>
 </template>
 
@@ -13,6 +61,7 @@
 import ButtonModal from '@/Components/ButtonModal.vue';
 import Modal from '@/Components/Modal.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { router } from '@inertiajs/vue3';
 
 export default {
 components: {
@@ -24,5 +73,27 @@ props: {
     canales: Array,
     usuarios: Array
 },
+data() {
+    return {
+        form: {
+            nombre: '',
+        },
+        editform: {
+            nombre: '',
+        },
+        id: null,
+    }
+},
+methods: {
+    crearCanal() {
+        router.post(route('canales.store'), this.form)
+    },
+    editarCanal() {
+        router.put(route('canales.update', this.id), this.editform)
+    },
+    eliminarCanal() {
+        router.delete(route('canales.destroy', this.id))
+    }
+}
 }
 </script>
--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/canales', [CanalesController::class, 'index'])->name('canales.index');
-    Route::get('/canales/create', [CanalesController::class, 'create'])->name('canales.create');
     Route::post('/canales', [CanalesController::class, 'store'])->name('canales.store');
     Route::get('/canales/{canales}/edit', [CanalesController::class, 'edit'])->name('canales.edit');
     Route::patch('/canales/{canales}', [CanalesController::class, 'update'])->name('canales.update');
