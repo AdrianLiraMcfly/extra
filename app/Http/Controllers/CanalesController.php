3e8e79b96914e08603570b58429f04d6c4e21f11
AdrianLiraMcfly <jorge.liralopez11@gmail.com>
2024-04-23 13:04:43 -0600

    Quiero bajar

--- app/Http/Controllers/CanalesController.php
+++ app/Http/Controllers/CanalesController.php
@@ -9,54 +9,5 @@ use Inertia\Response;
 
 class CanalesController extends Controller
 {
-    public function index(): Response
-    {
-        return Inertia::render('Canales/Index', [
-            'canales' => Canales::all(),
-        ]);
-    }
-
-    public function create(): Response
-    {
-        return Inertia::render('Canales/Create');
-    }
-
-    public function store(Request $request)
-    {
-        $request->validate([
-            'nombre' => 'required',
-            
-
-        ]);
-
-        Canales::create($request->all());
-
-        return redirect()->route('canales.index');
-    }
-
-    public function edit(Canales $canales): Response
-    {
-        return Inertia::render('Canales/Edit', [
-            'canales' => $canales,
-        ]);
-    }
-
-    public function update(Request $request, Canales $canales)
-    {
-        $request->validate([
-            'nombre' => 'required',
-            'descripcion' => 'required',
-        ]);
-
-        $canales->update($request->all());
-
-        return redirect()->route('canales.index');
-    }
-
-    public function destroy(Canales $canales)
-    {
-        $canales->delete();
-
-        return redirect()->route('canales.index');
-    }
+    
 }
--- resources/js/Pages/Forum/canales.vue
+++ resources/js/Pages/Forum/canales.vue
@@ -1,11 +1,19 @@
 <template>
-    
+    <AuthenticatedLayout>
+        <div class="container mx-auto">
+            <div class="flex justify-between items-center">
+                <h1 class="text-2xl font-bold">Canales</h1>
+
+            </div>
+        </div>
+    </AuthenticatedLayout>
 </template>
 
 <script>  
 import ButtonModal from '@/Components/ButtonModal.vue';
 import Modal from '@/Components/Modal.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { route } from 'laravel-jetstream';
 
 export default {
 components: {
