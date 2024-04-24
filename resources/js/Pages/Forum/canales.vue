a8c873a549809f4412922140e79208f50d6c7eaf
AlVielma <vielma7220>
2024-04-23 18:50:05 -0600

    mcflower

--- resources/js/Pages/Forum/canales.vue
+++ resources/js/Pages/Forum/canales.vue
@@ -33,9 +33,9 @@
                                         <td>
                                             <button-modal id_Modal="modalEditarCanal" class="btn btn-warning" boton="Editar" @click="editCanal(canal.id, canal.nombre)"></button-modal>
                                             <button-modal id_Modal="modalEliminarCanal" :class="canal.is_active == 1 ? 'btn btn-danger' : 'btn btn-success'" :boton="canal.is_active == 1 ? 'Desactivar' : 'Activar'" @click="obtenerCanal(canal.id)"></button-modal>
-                                           <!-- <Link :href="route('canales.show', canal.id)">
+                                           <Link :href="route('canales.show', canal.id)">
                                             <button type="button" class="btn btn-success">Ver</button>
-                                            </Link> -->
+                                            </Link> 
                                         </td>
                                     </tr>
                                 </tbody>
@@ -74,7 +74,7 @@
 <script>  
 import ButtonModal from '@/Components/ButtonModal.vue';
 import Modal from '@/Components/Modal.vue';
-//import { Link } from '@inertiajs/inertia-vue3';
+import { Link } from '@inertiajs/vue3'
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { router } from '@inertiajs/vue3';
 import axios from 'axios';
@@ -83,7 +83,7 @@ export default {
 components: {
     ButtonModal,
     Modal,
-    //Link,
+    Link,
     AuthenticatedLayout
 },
 props: {
